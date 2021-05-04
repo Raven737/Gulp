4d5e9d3fc504bbc94f952d0605a6e672711f9d0b
Raven737 <prvm79@gmail.com>
2021-05-04 12:08:32 +0300

    package-lock.json was deleted

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 
 let project_folder = "dist"; // Папка готового проєкту.
-let source_folder = "#src";  // Папка робочого проєкту.
+let source_folder = "#src";  // Папка початкового (робочого) проєкту.
 
-let path = {                 // Шлях до файлів.
-  build: {
+let path = {                 // Шляхи до файлів.
+  build: {                   // Production
     html: project_folder + "/",
     css: project_folder + "/css/",
     js: project_folder + "/js/",
     img: project_folder + "/img/",
     fonts: project_folder + "/fonts/",
   },
-  src: {
+  src: {                     // Source
     html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
     css: source_folder + "/scss/style.scss",
     js: source_folder + "/js/script.js",
     img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
     fonts: source_folder + "/fonts/*.ttf",
   },
-  watch: {
+  watch: {                   // Watching
     html: source_folder + "/**/*.html",
     css: source_folder + "/scss/**/*.scss",
     js: source_folder + "/js/**/*.js",
@@ -54,22 +54,22 @@ function browserSync(params) {
 }
 
 function html() {															// HTML з папки SRC -> в папку dist -> перезавантаження сторінки.
-  return src(path.src.html)										// Обираємо файл html, який знаходиться в папці #src.
+  return src(path.src.html)										// Обирає файл html (#src/.html, а файли _*.html НЕ обирає).
     .pipe(fileinclude())											// Збирає в один файл.
     .pipe(webphtml())                         // Інтегрує формат webp в html.
-    .pipe(dest(path.build.html))							// Вивантаження файлу html в папку dist.
-    .pipe(browsersync.stream())								// Перезавантажили сторінку.
+    .pipe(dest(path.build.html))							// Вивантаження файлу html в папку dist/.
+    .pipe(browsersync.stream())								// Перезавантажує сторінку.
 }
 
 function css() {															// SCSS -> CSS -> в dist/css -> перезавантаження сторінки.
-  return src(path.src.css)										// Обираємо файл scss, який знаходиться в папці #src.
+  return src(path.src.css)										// Обираємо файл style.scss (#src/scss/style.scss).
     .pipe(																		// scss -> css.
       scss({
         outputStyle: "expanded"
       })
     )
-    .pipe(
-      group_media()                           // Групує і вставляє медіа запити в кінець файлу.
+    .pipe(                                    // Групує і вставляє медіа запити в кінець файлу.
+      group_media()
     )
     .pipe(                                    // Додає автопрефікси.
       autoprefixer({
@@ -81,41 +81,41 @@ function css() {															// SCSS -> CSS -> в dist/css -> перезава
     .pipe(dest(path.build.css))								// Вивантаження файлу css в папку dist/css.
     .pipe(clean_css())                        // Оптимізує (очищає та стискає) css файл.
     .pipe(
-      rename({
-        extname: ".min.css"                   // Переіменовує файл -> додає до імя + ".min.css"
+      rename({                                // Переіменовує файл: додає до імя + ".min.css"
+        extname: ".min.css"
       })
     )
     .pipe(dest(path.build.css))								// Вивантаження файлу css в папку dist/css.
-    .pipe(browsersync.stream())								// Перезавантажили сторінку.
+    .pipe(browsersync.stream())								// Перезавантажує сторінку.
 }
 
-function js() {															  // HTML з папки SRC -> в папку dist -> перезавантаження сторінки.
-  return src(path.src.js)										  // Обираємо файл html, який знаходиться в папці #src.
+function js() {															  // JS з папки SRC -> в папку dist -> перезавантаження сторінки.
+  return src(path.src.js)										  // Обираємо файл script.js. (#src/js/script.js).
     .pipe(fileinclude())											// Збирає в один файл.
-    .pipe(dest(path.build.js))							  // Вивантаження файлу html в папку dist.
+    .pipe(dest(path.build.js))							  // Вивантаження файлу js в папку dist/js.
     .pipe(
       uglify()                                // Оптимізує (зтискає) файл.
     )
     .pipe(
       rename({
-        extname: ".min.js"                   // Переіменовує файл -> додає до імя + ".min.js"
+        extname: ".min.js"                   // Переіменовує файл: додає до імя + ".min.js".
       })
     )
-    .pipe(dest(path.build.js))							  // Вивантаження файлу html в папку dist.
-    .pipe(browsersync.stream())								// Перезавантажили сторінку.
+    .pipe(dest(path.build.js))							  // Вивантаження файлу js в папку dist/js.
+    .pipe(browsersync.stream())								// Перезавантажує сторінку.
 }
 
 function images() {													  // img з папки SRC -> в папку dist -> перезавантаження сторінки.
-  return src(path.src.img)										// Обираємо файл img, який знаходиться в папці #src.
-    .pipe(
+  return src(path.src.img)										// Обираємо файл img, який знаходиться в папці #src/img.
+    .pipe(                                    // Оптримізує зображення
       webp({
         quality: 70
       })
     )
-    .pipe(dest(path.build.img))						  	// Вивантаження файлу img в папку dist.
-    .pipe(src(path.src.img))										// Обираємо файл img, який знаходиться в папці #src.
+    .pipe(dest(path.build.img))						  	// Вивантаження файлу img в папку dist/img.
+    .pipe(src(path.src.img))									// Обираємо файл img, який знаходиться в папці #src/img.
     .pipe(
-      imagemin({
+      imagemin({                              // Стискає зображення.
         interlaced: true,
         progressive: true,
         optimizationLevel: 3,     // від 0 до 7.
@@ -126,7 +126,7 @@ function images() {													  // img з папки SRC -> в папку dist
         ]
       })
     )
-    .pipe(dest(path.build.img))						  	// Вивантаження файлу img в папку dist.
+    .pipe(dest(path.build.img))						  	// Вивантаження файлу img в папку dist/img.
     .pipe(browsersync.stream())								// Перезавантажили сторінку.
 }
 
@@ -144,18 +144,18 @@ gulp.task('svgSprite', function () {
 })
 
 function watchFiles(params) {
-  gulp.watch([path.watch.html], html);				// "Слідкує" за html, якщо є зміни - спрацьовує функція html().
-  gulp.watch([path.watch.css], css);					// "Слідкує" за css, якщо є зміни - спрацьовує функція css().
-  gulp.watch([path.watch.js], js);					  // "Слідкує" за js, якщо є зміни - спрацьовує функція js().
-  gulp.watch([path.watch.img], images);				// "Слідкує" за img, якщо є зміни - спрацьовує функція images().
+  gulp.watch([path.watch.html], html);	// "Слідкує" за html (#src/**/*.html), якщо є зміни - спрацьовує функція html().
+  gulp.watch([path.watch.css], css);		// "Слідкує" за css (#src/scss/**/*.scss), якщо є зміни - спрацьовує функція css().
+  gulp.watch([path.watch.js], js);			// "Слідкує" за js (#src/js/**/*.js), якщо є зміни - спрацьовує функція js().
+  gulp.watch([path.watch.img], images);	// "Слідкує" за img (#src/img/**/*.{jpg, png, svg, gif, ico, webp}), якщо є зміни - спрацьовує функція images().
 }
 
 function clean(params) {
   return del(path.clean);											// Видаляє папку dist.
 }
 
-let build = gulp.series(clean, gulp.parallel(html, css, js, images));			// Видаляє та виконує одночасно html, css та js.
-let watch = gulp.parallel(build, watchFiles, browserSync);
+let build = gulp.series(clean, gulp.parallel(html, css, js, images));	// Видаляє папку dist, а потім одночасно виконує функції html(), css(), js() та images().
+let watch = gulp.parallel(build, watchFiles, browserSync);            // 
 
 exports.html = html;
 exports.css = css;
@@ -164,3 +164,8 @@ exports.images = images;
 exports.build = build;
 exports.watch = watch;
 exports.default = watch;
+
+
+
+// series(1, 2, 3) запускає задачі по одній у вказаному порядку (1 -> 2 -> 3).
+// parallel(…) виконує задачи одночасно в любому порядку.
